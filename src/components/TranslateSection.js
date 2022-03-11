12d26b659b52757dde1a1b903b2b348e459da698
Nadav Shor <nadavs@nadavs-air.home>
2022-03-11 23:31:20 +0200

    added valedetions for inputs and cleaned side pannel component by placing each animation section into its own component

---
+++ src/components/TranslateSection.js
@@ -0,0 +1,172 @@
+import PlusIcon from "./plusIcon"
+import MinusIcon from "./minusIcon"
+import { useState } from "react"
+
+export default function TranslateSection(props) {
+
+    const [translateXFrom, setTranslateXFrom] = useState(0)
+    const [translateYFrom, setTranslateYFrom] = useState(0)
+    const [translateXTo, setTranslateXTo] = useState(0)
+    const [translateYTo, setTranslateYTo] = useState(0)
+
+    //limit number of input digits to the maxLength atrribute
+    const inputHandler = (e) => {
+        const { value, maxLength } = e.target;
+
+        if (String(value).length >= maxLength) {
+          e.preventDefault();
+          return;
+        }
+      };
+
+    const keyPressHandlerXF = (e) => {
+        const { value } = e.target;
+
+        if (value.charAt(0) === "0" && (e.key === "1" || e.key === "2" || e.key === "3" || e.key === "4" || e.key === "5" || e.key === "6" || e.key === "7" || e.key === "8" || e.key === "9" || e.key === "0")) {
+            setTranslateXFrom(String(translateXFrom).slice(1))
+            props.translatexf(String(translateXFrom).slice(1))
+        }
+
+        ["e", "E", "+"].includes(e.key) && e.preventDefault()
+
+        if (e.key === "Backspace" && value.length < 2) {
+            e.preventDefault()
+            setTranslateXFrom(0)
+            props.translatexf(0)
+        }
+    }
+
+    const keyPressHandlerXT = (e) => {
+        const { value } = e.target;
+
+        if (value.charAt(0) === "0" && (e.key === "1" || e.key === "2" || e.key === "3" || e.key === "4" || e.key === "5" || e.key === "6" || e.key === "7" || e.key === "8" || e.key === "9" || e.key === "0")) {
+            setTranslateXTo(String(translateXTo).slice(1))
+            props.translatext(String(translateXTo).slice(1))
+        }
+
+        ["e", "E", "+"].includes(e.key) && e.preventDefault()
+
+        if (e.key === "Backspace" && value.length < 2) {
+            e.preventDefault()
+            setTranslateXTo(0)
+            props.translatext(0)
+        }
+    }
+
+    const keyPressHandlerYF = (e) => {
+        const { value } = e.target;
+
+        if (value.charAt(0) === "0" && (e.key === "1" || e.key === "2" || e.key === "3" || e.key === "4" || e.key === "5" || e.key === "6" || e.key === "7" || e.key === "8" || e.key === "9" || e.key === "0")) {
+            setTranslateYFrom(String(translateYFrom).slice(1))
+            props.translateyf(String(translateYFrom).slice(1))
+        }
+
+        ["e", "E", "+"].includes(e.key) && e.preventDefault()
+
+        if (e.key === "Backspace" && value.length < 2) {
+            e.preventDefault()
+            setTranslateYFrom(0)
+            props.translateyf(0)
+        }
+    }
+
+    const keyPressHandlerYT = (e) => {
+        const { value } = e.target;
+
+        if (value.charAt(0) === "0" && (e.key === "1" || e.key === "2" || e.key === "3" || e.key === "4" || e.key === "5" || e.key === "6" || e.key === "7" || e.key === "8" || e.key === "9" || e.key === "0")) {
+            setTranslateYTo(String(translateYTo).slice(1))
+            props.translateyt(String(translateYTo).slice(1))
+        }
+
+        ["e", "E", "+"].includes(e.key) && e.preventDefault()
+
+        if (e.key === "Backspace" && value.length < 2) {
+            e.preventDefault()
+            setTranslateYTo(0)
+            props.translateyt(0)
+        }
+    }
+
+    return (
+        <div className="translate-controls-container">
+                    <h4 className="info-title">Translate</h4>
+
+                    <div className="sub-titles">
+                        <h6 className="info-title">Translate-X</h6>
+                        <h6 className="info-title">Translate-Y</h6>
+                    </div>
+
+                    <div className="controls-input-container">
+                        <div className="X-section">
+
+                            <div className="control-from">
+                                <p className="from-text">From</p>
+                                <div className="input-and-btns">
+                                    <PlusIcon add={() => {setTranslateXFrom(Number(translateXFrom) + 1); props.translatexf(Number(translateXFrom) + 1)}}/>
+                                    <input value={translateXFrom} 
+                                        type="number" 
+                                        className="counter-input" 
+                                        maxLength="4" 
+                                        onChange={(e) => {setTranslateXFrom(e.target.value); props.translatexf(e.target.value)}} 
+                                        onKeyPress={inputHandler} 
+                                        onKeyDown={keyPressHandlerXF}/>
+                                    <MinusIcon minus={() => {setTranslateXFrom(Number(translateXFrom) - 1); props.translatexf(Number(translateXFrom) - 1)}}/>
+                                </div>    
+                            </div>
+
+                            <div className="control-to">
+                                <p className="to-text">To</p>
+                                <div className="input-and-btns">
+                                    <PlusIcon add={() => {setTranslateXTo(Number(translateXTo) + 1); props.translatext(Number(translateXTo) + 1)}}/>
+                                    <input 
+                                        value={translateXTo} 
+                                        type="number" 
+                                        className="counter-input" 
+                                        maxLength="4" 
+                                        onChange={(e) => {setTranslateXTo(e.target.value); props.translatext(e.target.value)}} 
+                                        onKeyPress={inputHandler} 
+                                        onKeyDown={keyPressHandlerXT}/>
+                                    <MinusIcon minus={() => {setTranslateXTo(Number(translateXTo) - 1); props.translatext(Number(translateXTo) - 1)}}/>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className="Y-section">
+
+                            <div className="control-from">
+                                <p className="from-text">From</p>
+                                <div className="input-and-btns">
+                                    <PlusIcon add={() => {setTranslateYFrom(Number(translateYFrom) + 1); props.translateyf(Number(translateYFrom) + 1)}}/>
+                                    <input 
+                                        value={translateYFrom} 
+                                        type="number" 
+                                        className="counter-input" 
+                                        maxLength="4" 
+                                        onChange={(e) => {setTranslateYFrom(e.target.value); props.translateyf(e.target.value)}} 
+                                        onKeyPress={inputHandler} 
+                                        onKeyDown={keyPressHandlerYF}/>
+                                    <MinusIcon minus={() => {setTranslateYFrom(Number(translateYFrom) - 1); props.translateyf(Number(translateYFrom) - 1)}}/>
+                                </div>
+                            </div>
+
+                            <div className="control-to">
+                                <p className="to-text">To</p>
+                                <div className="input-and-btns">
+                                    <PlusIcon add={() => {setTranslateYTo(Number(translateYTo) + 1); props.translateyt(Number(translateYTo) + 1)}}/>
+                                    <input 
+                                        value={translateYTo} 
+                                        type="number" 
+                                        className="counter-input" 
+                                        maxLength="4" 
+                                        onChange={(e) => {setTranslateYTo(e.target.value); props.translateyt(e.target.value)}} 
+                                        onKeyPress={inputHandler} 
+                                        onKeyDown={keyPressHandlerYT}/>
+                                    <MinusIcon minus={() => {setTranslateYTo(Number(translateYTo) - 1); props.translateyt(Number(translateYTo) - 1)}}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+    )
+}
--- src/components/sidePannel.js
+++ src/components/sidePannel.js
@@ -1,16 +1,7 @@
 import "../styles/sidepannel.css"
-import PlusIcon from "./plusIcon"
-import MinusIcon from "./minusIcon"
-import { useState } from "react"
+import TranslateSection from "./TranslateSection"
 
 export default function SidePannel(props) {
-
-    const [translateXFrom, setTranslateXFrom] = useState("")
-    const [translateYFrom, setTranslateYFrom] = useState("")
-    const [translateXTo, setTranslateXTo] = useState("")
-    const [translateYTo, setTranslateYTo] = useState("")
-
-    // document.querySelector(':root').style.setProperty('--scaleX-to', "scaleX(5)") - changing css vars code
     
     //send the user input text to the animation pannel and animated
     const ForwardInputText = (e) => {
@@ -22,15 +13,6 @@ export default function SidePannel(props) {
         document.getElementById("animated-text").classList.add("play")
     }
 
-    //limit number of input digits to the maxLength atrribute
-    const inputHandler = (e) => {
-        const { value, maxLength } = e.target;
-        if (String(value).length >= maxLength) {
-          e.preventDefault();
-          return;
-        }
-      };
-
     return (
         <div className="side-pannel">
 
@@ -39,88 +21,14 @@ export default function SidePannel(props) {
                 <textarea className="animation-text-input" onChange={ForwardInputText} maxLength="100" placeholder="type here..." cols="30" rows="10"></textarea>
             </div>
 
-            <div className="translate-controls-container">
-                    <h4 className="info-title">Translate</h4>
+            <TranslateSection 
+                translatexf={(state) => document.querySelector(':root').style.setProperty('--translateX-from', `translateX(${state + "px"})`)} 
+                translatext={(state) => document.querySelector(':root').style.setProperty('--translateX-to', `translateX(${state + "px"})`)}
+                translateyf={(state) => document.querySelector(':root').style.setProperty('--translateY-from', `translateY(${state + "px"})`)}
+                translateyt={(state) => document.querySelector(':root').style.setProperty('--translateY-to', `translateY(${state + "px"})`)}
+                />
 
-                    <div className="sub-titles">
-                        <h6 className="info-title">Translate-X</h6>
-                        <h6 className="info-title">Translate-Y</h6>
-                    </div>
-
-                    <div className="controls-input-container">
-                        <div className="X-section">
-
-                            <div className="control-from">
-                                <p className="from-text">From</p>
-                                <div className="input-and-btns">
-                                    <PlusIcon add={() => setTranslateXFrom(Number(translateXFrom) + 1)}/>
-                                    <input value={translateXFrom} 
-                                        type="number" 
-                                        className="counter-input" 
-                                        maxLength="4" 
-                                        onChange={(e) => setTranslateXFrom(e.target.value)} 
-                                        onKeyPress={inputHandler} 
-                                        onKeyDown={(e) =>["e", "E", "+"].includes(e.key) && e.preventDefault()}/>
-                                    <MinusIcon minus={() => setTranslateXFrom(Number(translateXFrom) - 1)}/>
-                                </div>
-                            </div>
-
-                            <div className="control-to">
-                                <p className="to-text">To</p>
-                                <div className="input-and-btns">
-                                    <PlusIcon add={() => setTranslateXTo(Number(translateXTo) + 1)}/>
-                                    <input 
-                                        value={translateXTo} 
-                                        type="number" 
-                                        className="counter-input" 
-                                        maxLength="4" 
-                                        onChange={(e) => setTranslateXTo(e.target.value)} 
-                                        onKeyPress={inputHandler} 
-                                        onKeyDown={(e) =>["e", "E", "+"].includes(e.key) && e.preventDefault()}/>
-                                    <MinusIcon minus={() => setTranslateXTo(Number(translateXTo) - 1)}/>
-                                </div>
-                            </div>
-
-                        </div>
-
-                        <div className="Y-section">
-
-                            <div className="control-from">
-                                <p className="from-text">From</p>
-                                <div className="input-and-btns">
-                                    <PlusIcon add={() => setTranslateYFrom(Number(translateYFrom) + 1)}/>
-                                    <input 
-                                        value={translateYFrom} 
-                                        type="number" 
-                                        className="counter-input" 
-                                        maxLength="4" 
-                                        onChange={(e) => setTranslateYFrom(e.target.value)} 
-                                        onKeyPress={inputHandler} 
-                                        onKeyDown={(e) =>["e", "E", "+"].includes(e.key) && e.preventDefault()}/>
-                                    <MinusIcon minus={() => setTranslateYFrom(Number(translateYFrom) - 1)}/>
-                                </div>
-                            </div>
-
-                            <div className="control-to">
-                                <p className="to-text">To</p>
-                                <div className="input-and-btns">
-                                    <PlusIcon add={() => setTranslateYTo(Number(translateYTo) + 1)}/>
-                                    <input 
-                                        value={translateYTo} 
-                                        type="number" 
-                                        className="counter-input" 
-                                        maxLength="4" 
-                                        onChange={(e) => setTranslateYTo(e.target.value)} 
-                                        onKeyPress={inputHandler} 
-                                        onKeyDown={(e) =>["e", "E", "+"].includes(e.key) && e.preventDefault()}/>
-                                    <MinusIcon minus={() => setTranslateYTo(Number(translateYTo) - 1)}/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-            </div>
-
-            {/* <button className="trigger-animation-btn" onClick={triggerAnimation}>Play</button> */}
+            <button className="trigger-animation-btn" onClick={triggerAnimation}>Play</button>
         </div>
     )
 }
