6d4b80d54fe11dabee75acbc8cc4407b1d159be4
Nadav Shor <nadavs@nadavs-air.home>
2022-03-13 19:56:00 +0200

    added a side bar controls for mobile view (still in work)

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,12 @@ import logo from "../images/header-logo.svg"
 
 export default function Header() {
     return (
-       <header className="header">
-        <img src={logo} alt="LOGO" />
-        <div className="header-text-container">
-            <p className="header-text">Animation-Helper</p>
-            <p className="header-sub-text">By Nadav Shor</p>
-        </div>
-       </header> 
+        <header className="header">
+            <img src={logo} alt="LOGO" />
+            <div className="header-text-container">
+                <p className="header-text">Animation-Helper</p>
+                <p className="header-sub-text">By Nadav Shor</p>
+            </div>
+        </header> 
     )
 }
--- src/components/sidePannel.js
+++ src/components/sidePannel.js
@@ -5,6 +5,7 @@ import { useState } from "react"
 
 export default function SidePannel(props) {
 
+    // onject that will contain all animations data to be sent to code preview component
     const [animationsValues] = useState({translatexf: 0, translatext: 0, translateyf: 0, translateyt: 0})
 
     //getting the element that will be animated
@@ -21,10 +22,8 @@ export default function SidePannel(props) {
         props.animationsValue(animationsValues)
     }
 
-    // onject that will contain all animations data to be sent to code preview component
-    // const animationObject = {translatexf: 0, translatext: 0, translateyf: 0, translateyt: 0}
-
     return (
+        <>
         <div className="side-pannel">
 
             <div className="text-input-container">
@@ -53,5 +52,45 @@ export default function SidePannel(props) {
 
             <button className="trigger-animation-btn" onClick={triggerAnimation}>Play</button>
         </div>
+
+        <div className="hamburger-menu">
+            <input id="menu__toggle" type="checkbox" />
+            <label className="menu__btn" htmlFor="menu__toggle">
+            <span></span>
+            </label>
+
+            <ul className="menu__box">
+                <li className="menu__item">
+                    <div className="side-pannel">
+                        <div className="text-input-container">
+                            <h4 className="text-input-info">Write Something To Animate</h4>
+                            <textarea className="animation-text-input" onChange={ForwardInputText} maxLength="100" placeholder="type here..." cols="30" rows="10"></textarea>
+                        </div>
+
+                        <TranslateSection 
+                            translatexf={(state) => {
+                                document.querySelector(':root').style.setProperty('--translateX-from', `translateX(${state + "px"})`)
+                                if (state !== 0 || state !== "0") {animationsValues.translatexf = state}
+                                }} 
+                            translatext={(state) => {
+                                document.querySelector(':root').style.setProperty('--translateX-to', `translateX(${state + "px"})`)
+                                if (state !== 0 || state !== "0") {animationsValues.translatext = state}
+                                }}
+                            translateyf={(state) => {
+                                document.querySelector(':root').style.setProperty('--translateY-from', `translateY(${state + "px"})`)
+                                if (state !== 0 || state !== "0") {animationsValues.translateyf = state}
+                                }}
+                            translateyt={(state) => {
+                                document.querySelector(':root').style.setProperty('--translateY-to', `translateY(${state + "px"})`)
+                                if (state !== 0 || state !== "0") {animationsValues.translateyt = state}
+                                }}
+                            />
+
+                        <button className="trigger-animation-btn" onClick={triggerAnimation}>Play</button>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        </>
     )
 }
